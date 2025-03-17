3e651a5db2c01afea3af74537643fa79065960b6
Ayushman2312 <108574597+Ayushman2312@users.noreply.github.com>
2025-03-18 00:06:58 +0530

    Update settings.py

--- onetool/settings.py
+++ onetool/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'onetool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '1matrix',
+        'USER': 'ayushman',
+        'PASSWORD': 'Ayushman@23122003',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
